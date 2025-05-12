824bab0776fc0f3de2df77f5e7b7d4555223d01b
j742830 <moritz.rose@extern.f-i.de>
2025-05-12 22:38:20 +0200

    corrected basepath /ivr/...

--- src/main/java/de/fi/IspMockServer/controller/IvrController.java
+++ src/main/java/de/fi/IspMockServer/controller/IvrController.java
@@ -1,7 +1,9 @@
 package de.fi.IspMockServer.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/ivr")
+@RestController
+@RequestMapping("/agent")
 public class IvrController {
 }
