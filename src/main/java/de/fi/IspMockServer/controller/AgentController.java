f699f86a36cba6c46f9f7b13c26a7090bbbe5ab2
j742830 <moritz.rose@extern.f-i.de>
2025-05-12 14:21:12 +0200

    rename UserAgentController -> AgentController

--- src/main/java/de/fi/IspMockServer/controller/AgentController.java
+++ src/main/java/de/fi/IspMockServer/controller/AgentController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class UserAgentController {
+@RestController("/agent")
+public class AgentController {
 
     @GetMapping("/grammar")
     public String grammar() {
--- src/main/java/de/fi/IspMockServer/controller/IvrController.java
+++ src/main/java/de/fi/IspMockServer/controller/IvrController.java
@@ -1,4 +1,7 @@
 package de.fi.IspMockServer.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/ivr")
 public class IvrController {
 }
