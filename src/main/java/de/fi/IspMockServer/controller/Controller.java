59f8b52cc54ec6fa16edd87e3aaa424d285295d2
j742830 <moritz.rose@extern.f-i.de>
2025-05-12 10:21:12 +0200

    initial commit

---
+++ src/main/java/de/fi/IspMockServer/controller/Controller.java
@@ -0,0 +1,26 @@
+package de.fi.IspMockServer.controller;
+
+
+import de.fi.IspMockServer.Event;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/grammar")
+    public String grammar() {
+        return "Grammatik-Beispiel";
+    }
+
+    @PostMapping("/event")
+    public String event(@RequestBody Event event) {
+        if (event.getType().equals(Event.RINGING)) {
+            return "Es klingelt!";
+        } else {
+            return "Es macht was, was nicht Klingeln ist.";
+        }
+    }
+}
---
+++ src/test/java/de/fi/IspMockServer/IspMockServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fi.IspMockServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IspMockServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
