7e5891d49746bda03d1ae85b3ca6b657331c9171
Alexander Holthaus <Holti91@web.de>
2025-06-18 09:57:43 +0200

    add vxml with record

--- src/main/java/de/fi/IspMockServer/IspMockServerApplication.java
+++ src/main/java/de/fi/IspMockServer/IspMockServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IspMockServerApplication {
 
 	public static void main(String[] args) {
+		//PW Openeye: FdbauZ7_FwfvaL0x
 		SSLDisableExample.execute();
 		SpringApplication.run(IspMockServerApplication.class, args);
 	}
