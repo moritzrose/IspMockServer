090d39075ceb6e963b4268894722d7017cd45f33
j742830 <moritz.rose@extern.f-i.de>
2025-06-20 17:26:43 +0200

    minor stuff

---
+++ src/main/java/de/fi/IspMockServer/controller/SseController.java
@@ -0,0 +1,41 @@
+package de.fi.IspMockServer.controller;
+
+import de.fi.IspMockServer.emitter.CustomSseEmitter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/sses")
+public class SseController {
+
+    public static final Map<String, CustomSseEmitter> emitters = new HashMap<>();
+
+    @GetMapping("/{agentId}")
+    public SseEmitter connect(@PathVariable String agentId) {
+        final CustomSseEmitter emitter = new CustomSseEmitter(0L);
+
+        emitter.onCompletion(() -> {
+            emitter.complete();
+            emitters.remove(agentId);
+        });
+
+        emitter.onTimeout(() -> {
+            emitter.complete();
+            emitters.remove(agentId);
+        });
+
+        emitter.onError((e) -> {
+            emitter.completeWithError(e);
+            emitters.remove(agentId);
+        });
+
+        emitters.put(agentId, emitter);
+        return emitter;
+    }
+}
