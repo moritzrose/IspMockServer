352ec0c9c5582605bb0e85debf42b96fd41edc62
moritzrose <moritz_rose@yahoo.de>
2025-06-21 10:05:00 +0200

    fix

--- src/main/java/de/fi/IspMockServer/controller/SseController.java
+++ src/main/java/de/fi/IspMockServer/controller/SseController.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/sse")
+@RequestMapping("/sse-stream")
 public class SseController {
 
     public static final Map<String, CustomSseEmitter> emitters = new HashMap<>();
